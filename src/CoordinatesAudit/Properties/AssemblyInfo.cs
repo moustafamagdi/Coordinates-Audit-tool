3218af84874523ead9fa03635d37e9f73dadd3e1
moustafamagdi <moustafa.magdi@gmail.com>
2026-09-03 11:14:44 +0300

    chore: add M0 foundation files

---
+++ src/CoordinatesAudit/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Revit Coordinate Auditor")]
+[assembly: AssemblyDescription("Read-only coordinate auditing tools for Autodesk Revit.")]
+[assembly: AssemblyCompany("Moustafa Magdi")]
+[assembly: AssemblyProduct("Revit Coordinate Auditor")]
+[assembly: AssemblyCopyright("Copyright © Moustafa Magdi")]
+[assembly: ComVisible(false)]
+[assembly: Guid("c4b218c4-85e0-43d1-bad7-0d5eb35bc0b9")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
