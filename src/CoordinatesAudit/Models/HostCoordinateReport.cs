4da477d3567550645a9dde6d059b6ced79394342
moustafamagdi <moustafa.magdi@gmail.com>
2026-09-03 11:19:23 +0300

    feat: add host coordinate reader

---
+++ src/CoordinatesAudit/Models/HostCoordinateReport.cs
@@ -0,0 +1,16 @@
+namespace CoordinatesAudit.Models
+{
+    public sealed class HostCoordinateReport
+    {
+        public string ModelTitle { get; set; }
+        public string ModelPath { get; set; }
+        public string RevitBuild { get; set; }
+        public string LengthUnit { get; set; }
+        public string ProjectLocationName { get; set; }
+        public string AngleToTrueNorth { get; set; }
+        public string SharedOriginPosition { get; set; }
+        public CoordinatePointData ProjectBasePoint { get; set; }
+        public CoordinatePointData SurveyPoint { get; set; }
+        public string InternalOriginPosition { get; set; }
+    }
+}
